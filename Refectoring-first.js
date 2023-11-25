b4450b034f333c9e0cb6f908f36aa186192e7a6a
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-11-25 12:12:36 +0900

    변수명 명확히하기

--- Refectoring-first.js
+++ Refectoring-first.js
@@ -15,32 +15,32 @@ function format(num) {
 
 /**
  * 금액 계산기
- * @param {*} perf 각 공연 관련 값
+ * @param {*} aPerformance 각 공연 관련 값
  * @param {*} play 각 공연 시나리오 개별값
  */
-function amountFor(perf, play) {
-  let thisAmount = 0; // 각 공연의 금액
+function amountFor(aPerformance, play) {
+  let result = 0; // 각 공연의 금액
 
   switch (play.type) {
     case "tragedy": // 비극
-      thisAmount = 40000;
-      if (perf.audience > 30) {
+      result = 40000;
+      if (aPerformance.audience > 30) {
         // 관객이 30을 넘길 경우
-        thisAmount += 1000 * (perf.audience - 30);
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy": // 희극
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
 
-  return thisAmount;
+  return result;
 }
 
 /**

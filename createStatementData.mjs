feee190182c3be8181eb118a37e289c1a9e8b380
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-11-26 11:23:27 +0900

    amountFor()함수의 공연료 계산로직 클래스화 테스트

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -1,4 +1,38 @@
 /**
+ * 공연료 계산을 위한 로직 클래스
+ */
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance; // 공연
+    this.play = aPlay; // 공연정보
+  }
+  get amount() {
+    let result = 0; // 각 공연의 금액
+
+    switch (this.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          // 관객이 30을 넘길 경우
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+
+    return result;
+  }
+}
+
+/**
  * 중간 데이터 생성 함수
  * @param {*} invoice 공연 청구서
  * @param {*} plays 공연 시나리오 정보
@@ -13,6 +47,11 @@ export default function createStatementData(invoice, plays) {
 
   // 얕은복사 함수
   function enrichPerformance(aPerformance) {
+    // 공연료 계산을 위한 로직 클래스
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result); // 연극정보
     result.amount = amountFor(result); // 금액 계산
@@ -31,32 +70,11 @@ export default function createStatementData(invoice, plays) {
   /**
    * 금액 계산 함수
    * @param {*} aPerformance 각 공연 관련 값
-   * @param {*} play 각 공연 시나리오 개별값
    * @returns 금액 계산값
    */
   function amountFor(aPerformance) {
-    let result = 0; // 각 공연의 금액
-
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          // 관객이 30을 넘길 경우
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
 
   /**
